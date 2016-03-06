492fd4d1c0f9a26782c6fbb0866c048074cd4db7
Administrator <416449023@qq.com>
2016-03-06 16:12:20 +0800

    1、结算中心具体功能实现

--- jingdong/src/main/java/com/jerry/jingdong/activity/BalanceAccountsCenterActivity.java
+++ jingdong/src/main/java/com/jerry/jingdong/activity/BalanceAccountsCenterActivity.java
@@ -4,13 +4,16 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -21,6 +24,7 @@ import com.jerry.jingdong.R;
 import com.jerry.jingdong.conf.MyConstants;
 import com.jerry.jingdong.entity.BalanceAccountCenterInfo;
 import com.jerry.jingdong.utils.UIUtils;
+import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.util.List;
@@ -66,8 +70,6 @@ public class BalanceAccountsCenterActivity extends Activity implements View.OnCl
     TextView mBalanceAccountTvBillType;
     @Bind(R.id.balance_account_rl_bill)
     RelativeLayout mBalanceAccountRlBill;
-    @Bind(R.id.balance_account_tv_product_num)
-    TextView mBalanceAccountTvProductNum;
     @Bind(R.id.balance_account_ll_iv_product_container)
     LinearLayout mBalanceAccountLlIvProductContainer;
     @Bind(R.id.balance_account_rl_product_detial)
@@ -114,14 +116,55 @@ public class BalanceAccountsCenterActivity extends Activity implements View.OnCl
                         String resJson = response.body().string();
                         parseJson(resJson);
                     } else {
-                        Toast.makeText(BalanceAccountsCenterActivity.this, "数据请求失败", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(BalanceAccountsCenterActivity.this, "数据请求失败", Toast.LENGTH_SHORT).show();
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
-                    Toast.makeText(BalanceAccountsCenterActivity.this, "数据请求失败", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(BalanceAccountsCenterActivity.this, "数据请求失败", Toast.LENGTH_SHORT).show();
                 }
             }
         }.start();
+
+    }
+
+    /**
+     * 当数据加载成功的时候就去给界面设置数据
+     */
+    private void setDataRefreshView() {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                List<BalanceAccountCenterInfo.ProductList> productList = mAccountCenterInfo.productList;
+                for (int i = 0; i < productList.size(); i++) {
+                    ImageView iv = new ImageView(UIUtils.getContext());
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
+                    if (i != 0) {
+                        params.leftMargin = 5;
+                    }
+                    String url = MyConstants.URL.BASEURL + productList.get(i).product.pic;
+                    Picasso.with(UIUtils.getContext()).load(url).into(iv);
+                    mBalanceAccountLlIvProductContainer.addView(iv, params);
+                }
+
+                TextView tv = new TextView(UIUtils.getContext());
+                tv.setTextColor(Color.parseColor("#000000"));
+                LinearLayout.LayoutParams tvParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                tvParams.gravity = Gravity.CENTER_VERTICAL;
+                tv.setText("共" + mAccountCenterInfo.checkoutAddup.totalCount + "件");
+                mBalanceAccountLlIvProductContainer.addView(tv, tvParams);
+                mBalanceAccountTvProductMoney.setText("￥" + mAccountCenterInfo.checkoutAddup.totalPrice);
+                mBalanceAccountTvFreight.setText("+￥" + mAccountCenterInfo.checkoutAddup.freight);
+                List<String> checkoutProm = mAccountCenterInfo.checkoutProm;
+                for (int i = 0; i < checkoutProm.size(); i++) {
+                    TextView tvEnjoy = new TextView(UIUtils.getContext());
+                    tvEnjoy.setTextColor(Color.parseColor("#000000"));
+                    tvEnjoy.setText(checkoutProm.get(i));
+                    mBalanceAccountLlEnjoyContainer.addView(tvEnjoy);
+                }
+
+            }
+        });
 
     }
 
@@ -133,6 +176,9 @@ public class BalanceAccountsCenterActivity extends Activity implements View.OnCl
     private void parseJson(String resJson) {
         Gson gson = new Gson();
         mAccountCenterInfo = gson.fromJson(resJson, BalanceAccountCenterInfo.class);
+        if (mAccountCenterInfo != null) {
+            setDataRefreshView();
+        }
     }
 
 
@@ -178,9 +224,8 @@ public class BalanceAccountsCenterActivity extends Activity implements View.OnCl
                 Toast.makeText(this, "商品明细", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.balance_account_btn_commit://提交按钮
-                Toast.makeText(this, "提交订单", Toast.LENGTH_SHORT).show();
-                commitData();
-
+//                Toast.makeText(this, "提交订单", Toast.LENGTH_SHORT).show();
+                commitData();//提交订单
                 break;
         }
     }
@@ -206,11 +251,12 @@ public class BalanceAccountsCenterActivity extends Activity implements View.OnCl
                             @Override
                             public void run() {
                                 Toast.makeText(BalanceAccountsCenterActivity.this, "订单提交成功", Toast.LENGTH_SHORT).show();
+                                Intent commitIntent = new Intent(UIUtils.getContext(), CommitOrderActivity.class);
+                                startActivity(commitIntent);
                             }
                         });
 
-                        Intent commitIntent = new Intent(UIUtils.getContext(), CommitOrderActivity.class);
-                        startActivity(commitIntent);
+
                     } else {
 
                         runOnUiThread(new Runnable() {
@@ -257,6 +303,7 @@ public class BalanceAccountsCenterActivity extends Activity implements View.OnCl
      * 创建选择支付方式的对话框
      */
     private void createDialog2PayWay() {
+
         final AlertDialog.Builder payBuilder = new AlertDialog.Builder(this);
         View view = View.inflate(this, R.layout.pay_dialog_view, null);
         payBuilder.setCustomTitle(view);
