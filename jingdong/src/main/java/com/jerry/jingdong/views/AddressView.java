e85bdb77657bef6a74ca3948b91cd17991bc8225
Administrator <888888>
2016-03-06 08:51:40 +0800

    资源添加

---
+++ jingdong/src/main/java/com/jerry/jingdong/views/AddressView.java
@@ -0,0 +1,22 @@
+package com.jerry.jingdong.views;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jerry.jingdong.base.MyBaseAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/3/6.
+ */
+public class AddressView extends MyBaseAdapter{
+    public AddressView(List datas) {
+        super(datas);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
